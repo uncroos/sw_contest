978dd01baea5468b2a0c124a829f0e30d5a03c8c
uncroos <any492266@gmail.com>
2024-10-23 23:13:22 +0900

    시작

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # sw_contest
+
+## 1. 가상환경
+
+- 가상환경 설치
+
+  ```
+  python3 -m venv venv
+  ```
+
+- 가상환경 활성화
+  ```
+  source venv/bin/activate
+  ```
+
+## 2. 사용한 라이브러리 설치
+
+    pip install pandas matplotlib seaborn
---
+++ train.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# train.csv 파일 읽기
+file_path = '/content/drive/MyDrive/Colab Notebooks/sw_contest/data/data/train.csv'  # 파일 경로 수정
+data = pd.read_csv(file_path)
+
+# 시각화 스타일 설정
+plt.figure(figsize=(8, 6))
+sns.set(style="whitegrid")
+
+# 'fake', 'real' 카테고리 값의 빈도수 계산
+category_counts = data['label'].value_counts()
+
+# 막대 그래프 (Seaborn 사용)
+ax = sns.barplot(x=category_counts.index, y=category_counts.values, hue=category_counts.index,
+                 palette="coolwarm", dodge=False, legend=False)
+
+# 그래프에 수치 표시
+for p in ax.patches:
+    ax.annotate(f'{p.get_height():.0f}',
+                (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center', fontsize=12, color='black', xytext=(0, 5),
+                textcoords='offset points')
+
+# 제목과 레이블 설정
+plt.title('Fake and Real', fontsize=16, weight='bold')
+plt.xlabel('Category', fontsize=14)
+plt.ylabel('Count', fontsize=14)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+# 그래프 출력
+plt.show()
